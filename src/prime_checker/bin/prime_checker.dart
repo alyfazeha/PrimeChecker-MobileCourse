bfb213c9c26ea314b8528e3652d8ed5f50e71bcd
alyfazeha <alyfazeha@gmail.com>
2026-03-11 11:49:31 +0700

    Implement prime number detection algorithm using loops

--- src/prime_checker/bin/prime_checker.dart
+++ src/prime_checker/bin/prime_checker.dart
@@ -1,5 +1,30 @@
 import 'package:prime_checker/prime_checker.dart' as prime_checker;
 
 void main(List<String> arguments) {
-  print('Hello world: ${prime_checker.calculate()}!');
+print('=== Program Pengecek Bilangan Prima ===');
+  // Simulasi input beberapa angka
+  cekDanSimpan(7,);  
+  cekDanSimpan(10,); 
+  cekDanSimpan(2,);  
+  cekDanSimpan(1,);  
+}
+
+void cekDanSimpan(int angka) {
+  bool isPrima = true;
+
+  // Logika Bilangan Prima
+  if (angka < 2) {
+    isPrima = false;
+  } else {
+    // PERULANGAN: Mengecek apakah angka habis dibagi bilangan lain
+    for (int i = 2; i <= angka ~/ 2; i++) {
+      if (angka % i == 0) {
+        isPrima = false; // Jika habis dibagi, maka bukan prima
+        break;
+      }
+    }
+  }
+
+  // PERCABANGAN: Menentukan teks status berdasarkan hasil cek
+  String status = isPrima ? "Adalah Bilangan Prima" : "Bukan Bilangan Prima";
 }
